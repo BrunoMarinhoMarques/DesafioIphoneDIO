fe1aa0366b6af9a6df1b2589b15cdd7a40fabae6
Bruno Henrique Marinho Marques <brunohenriw1@gmail.com>
2023-10-11 14:32:48 -0300

    Projeto do Iphone para o desafio DIO

---
+++ desafioIphoneDio/src/Iphone.java
@@ -0,0 +1,55 @@
+public class Iphone implements AparelhoTelefonico, NavegadorInternet, ReprodutorMusical {
+
+   
+    public void atender() {
+        System.out.println("Atendendo telefone");
+    }
+
+
+    public void iniciarCorreioVoz() {
+        System.out.println("Iniciando correio de Voz");
+        
+    }
+
+
+    public void ligar() {
+       System.out.println("Ligando");
+        
+    }
+
+ 
+    public void adicionarNovaAba() {
+       System.out.println("Adicionando nova aba");
+        
+    }
+
+
+    public void atualizarPagina() {
+        System.out.println("Atualizando pagina");
+        
+    }
+
+
+    public void exibirPagina() {
+      System.out.println("Exibindo pagina");
+        
+    }
+
+    public void pausar() {
+       System.out.println("Pausando musica");
+        
+    }
+
+  
+    public void selecionarMusica() {
+       System.out.println("Selecionando musica");
+        
+    }
+
+    public void tocar() {
+       System.out.println("Tocando musica");
+        
+    }
+   
+    
+}
